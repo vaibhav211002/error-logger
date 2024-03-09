cfddbbfed768df696f3a91753ffd0f94b1bf786d
Vaibhav <vaibhavbhatt9666@gmail.com>
2024-03-09 22:38:05 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+// errorHandler.js
+
+const sendErrorToServer = (errorMessage) => {
+    console.log(errorMessage);
+    fetch('http://localhost:3000/server', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json' 
+      },
+      body: JSON.stringify({ error: errorMessage })
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to send error to server');
+      }
+      console.log('Error sent to server successfully');
+    })
+    .catch(error => {
+      console.error('Error sending error to server:', error);
+    });
+  };
+  
+  const handleError = () => {
+    const originalConsoleError = console.error;
+    console.error = (...args) => {
+      if (args[0].includes('[hmr]')) {
+        console.log('HMR error detected:', args[0]);
+        if (args[0]) {
+          sendErrorToServer(args[0]);
+          console.log('yes error was here');
+        } else {
+          sendErrorToServer('No Error Was Detected');
+        }
+        console.log(args[0]);
+      } else {
+        originalConsoleError(...args);
+      }
+    };
+  
+    window.onerror = function(message, source, lineno, colno, error) {
+      const errorMessage = {
+        message,
+        source,
+        lineno,
+        colno,
+        error: error ? error.toString() : null
+      };
+      console.log('Error generated:', errorMessage);
+  
+      if (errorMessage !== '') {
+        sendErrorToServer(errorMessage);
+        console.log('yes error was here');
+      } else {
+        sendErrorToServer('No Error Was Detected');
+      }
+    };
+  };
+  
+  module.exports =  handleError ;
+  
